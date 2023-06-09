f9d5ea8422f15f02e6dab76dc3245207e865828e
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2023-06-09 10:45:16 +0600

    [Done] create user route and auto generate increment id , default password

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import userRouter from './app/modules/users/users.route'
 
-const app:Application = express();
-const port = 3000;
+const app: Application = express()
+// const port = 3000;
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Application routes
+app.use('/api/v1/users/', userRouter)
 
 //Testing
-app.get('/', (req:Request, res:Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-
-
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,19 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config/index';
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database connection successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database connection successfully`)
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-          })
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`Fail to connect database`, error)
+  }
 
-    } catch (error) {
-        console.log(`Fail to connect database`,error);
-
-
-    }
-
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-bootstrap();
-
-
+bootstrap()
